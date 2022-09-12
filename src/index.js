8bdfddf058cd8542e951301d40545d6a8cbcb55a
Jules Charles <j.charles@bweez.com>
2022-09-12 17:08:23 +0200

    création first wilder au début

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const typeorm = require('typeorm')
+const Wilder = require('./models/Wilder')
 
 const app = express()
 
@@ -7,6 +8,7 @@ const dataSource = new typeorm.DataSource({
     type: 'sqlite',
     database: 'db.sqlite',
     synchronize: true, // utile pour debug et dev, le jour ou l'app est en prod ne plus faire ca puisque que c'est la destruction, ce qui nous evite a devoir ecrire des fichiers de migration
+    entities: [require('./models/Wilder')]
 })
 
 app.get('/', function (req, res) {
@@ -17,6 +19,7 @@ const PORT = 4000
 
 const start = async () => {
     await dataSource.initialize() //attend que la data soit bien initialize avant de listen (.then)
+    dataSource.getRepository(Wilder).save({name: 'First Wilder'})
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     })
